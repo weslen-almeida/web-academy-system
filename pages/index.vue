8a0bb217e9bfb90183bb1c61c4768fa1e47484af
Weslen Almeida <weslengomes@gmail.com>
2024-01-10 06:58:24 -0300

    fix: adicionando o tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,23 @@
+
 <template>
-    <div>
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p class="text-gray-700 dark:text-gray-300">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+    <div class="flex gap-2">
         Home page
     </div>
     <div>
-        Api Response: {{ data }}
+        Api Response:
     </div>
+    
 </template>
 
 <script setup lang="ts">
 import { buildApiUrl } from "@/utils/services/fetch-utils";
+
 
 const { data } = await useFetch(
     buildApiUrl('/user'), {
@@ -18,8 +27,9 @@ const { data } = await useFetch(
 
 console.log(data)
 
+
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
