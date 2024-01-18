d43fe09e0183242983a28bb5aad02243d9fa768b
Weslen Almeida <weslengomes@gmail.com>
2024-01-18 16:22:46 -0300

    fix(menu):  inserindo a parte do logo do sistema

--- components/layout/Aside.vue
+++ components/layout/Aside.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 
+const props = defineProps({
+  menuToggle: {
+    type: Boolean,
+    required: true
+  }
+})
+
 const menu = ref([
   {
     label: 'Inicio',
@@ -21,10 +28,19 @@ const menu = ref([
 </script>
 
 <template>
-  <aside class="w-60 bg-zinc-800 py-6 px-2">
-    <!-- Colocar a logo aqui -->
+  <aside class="bg-zinc-800 py-6 px-2" :class="props.menuToggle ? 'w-60': 'w-20'">
 
-    <!-- Menu -->
-    <UVerticalNavigation :links="menu" />
+    <div class="mt-1 ml-2 text-center">
+      <span v-if="props.menuToggle" >
+        System Academy
+      </span>
+      <span v-else>
+        S.A
+      </span>
+    </div>
+
+    <div class="mt-8">
+      <UVerticalNavigation :links="menu" />
+    </div>
   </aside>
 </template>
